0a8ef14e84807911150580f6a91af2bd37d76d0c
Pyae Soan Aung <server01.psa1981@gmail.com>
2023-08-28 10:00:06 +0630

    03 complete

---
+++ 03 Functions/04/namespace3_avoid.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+/*
+double pow(double x, int exp)
+{
+	std::cout<< "Hello " << std::endl;
+	return 3.14;
+}
+*/
+int main()
+{
+	cout << pow(2.0,2) << endl;
+
+
+	return 0;
+}
+
