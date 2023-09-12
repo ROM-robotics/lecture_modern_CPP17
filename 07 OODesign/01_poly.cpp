6b15e9864c3676ef8f09d8084de33df2b9b2b5d5
Pyae Soan Aung <server01.psa1981@gmail.com>
2023-09-12 09:50:48 +0630

    start 07

---
+++ 07 OODesign/01_poly.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class Printable {
+public:
+    virtual void print() const= 0; // must override
+    // virtual void paint() const= 0;
+};
+
+class Printer : public Printable {
+public:
+    void print() const override{
+        cout << "Printer" << endl;
+    }
+};
+
+class Scanner : public Printable {
+public:
+    void print() const override{
+        cout << "Scanner" << endl;
+    }
+};
+
+void Print(const Printable& p) {
+    p.print();
+}
+int main() {
+    Printable *printer = new Printer();
+    Printable *scanner = new Scanner();
+
+    printer->print();
+    scanner->print();
+
+    // Print(Printer());
+    // Print(Scanner());
+
+    return 0;
+}
