394364f72dbde40f2c43061b6c2206b53bc15632
Pyae Soan Aung <server01.psa1981@gmail.com>
2023-09-16 22:25:28 +0630

    update

---
+++ 01 Setting up C++/04_cmake_ws/build/CMakeFiles/main.dir/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/main.dir/main.cpp.o: \
+ /home/mr_robot/Desktop/lecture_modern_CPP17/01\ Setting\ up\ C++/04_cmake_ws/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/mr_robot/Desktop/lecture_modern_CPP17/01\ Setting\ up\ C++/04_cmake_ws/tools.hpp
